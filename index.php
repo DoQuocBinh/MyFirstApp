aa917930cf283c01aa88331f736a6135600f5534
Binh Do Quoc <binhdq@fpt.edu.vn>
2019-04-25 14:48:28 +0700

    1

--- index.php
+++ index.php
@@ -30,9 +30,7 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["name"] .  "<br>";
-    }
+    echo "rows";
 } else {
     echo "0 results";
 }
