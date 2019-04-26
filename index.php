66ae6f4b3b26e733c471c5d3c469c870ad6417c3
Binh Do Quoc <binhdq@fpt.edu.vn>
2019-04-26 17:00:38 +0700

    11

--- index.php
+++ index.php
@@ -4,21 +4,7 @@
 
 <h1>My first PHP page</h1>
 
-<?php
-echo "Hello abc XYZ!";
-?>
-
-<?php
-$txt1 = "Learn PHP";
-$txt2 = "FPT Greenwich";
-$x = 5;
-$y = 5;
-
-echo "<h2>" . $txt1 . "</h2>";
-echo "Study PHP at " . $txt2 . "<br>";
-echo $x + $y;
-
- 
+<?php 
 $sql = "SELECT id, name FROM label";
 
 $db = parse_url(getenv("DATABASE_URL"));
