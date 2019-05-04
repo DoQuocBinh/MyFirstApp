ad76be6b9e8f2f5fe68364c69aefbd845b44a24d
Binh Do Quoc <binhdq@fpt.edu.vn>
2019-05-04 10:20:24 +0700

    11

--- insertuser.php
+++ insertuser.php
@@ -8,7 +8,25 @@
 </form>
 <?php
 	if (isset($_POST["id"]) && isset($_POST["name"])) {
-		echo "hidfds";
+		var $eid = $_POST["id"];
+		var $name = $_POST["name"];
+		//khoi tao ket noi den csdl
+		$db = parse_url(getenv("DATABASE_URL"));
+   		$pdo = new PDO("pgsql:" . sprintf(
+        	"host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")));
+
+        $data = [
+		    'id' => $eid,
+		    'name' => $name
+		];
+		$sql = "INSERT INTO label (id, name) VALUES (:id, :name)";
+		$stmt= $pdo->prepare($sql);
+		$stmt->execute($data);
 
 	}
 ?>
