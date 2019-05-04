06d6b48a2c9011fe38d8bd09b797305c7de9826d
Binh Do Quoc <binhdq@fpt.edu.vn>
2019-05-04 10:30:20 +0700

    111

--- insertuser.php
+++ insertuser.php
@@ -8,18 +8,22 @@
 </form>
 <?php
 	if (isset($_POST["id"]) && isset($_POST["name"])) {
-		$eid = $_POST["id"];
-		$name = $_POST["name"];
-		//khoi tao ket noi den csdl
-		$db = parse_url(getenv("DATABASE_URL"));
-   		$pdo = new PDO("pgsql:" . sprintf(
-        	"host=%s;port=%s;user=%s;password=%s;dbname=%s",
-        $db["host"],
-        $db["port"],
-        $db["user"],
-        $db["pass"],
-        ltrim($db["path"], "/")));
-
+        $eid = $_POST["id"];
+        $name = $_POST["name"];
+        //khoi tao ket noi den csdl
+        if (empty(getenv("DATABASE_URL"))){
+            $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=myDB', 'postgres', '123456');
+        }  else {
+           $db = parse_url(getenv("DATABASE_URL"));
+           $pdo = new PDO("pgsql:" . sprintf(
+                "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+                $db["host"],
+                $db["port"],
+                $db["user"],
+                $db["pass"],
+                ltrim($db["path"], "/")
+           ));
+        } 
         $data = [
 		    'id' => $eid,
 		    'name' => $name
