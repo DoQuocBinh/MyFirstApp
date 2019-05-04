4884dc562e3c6478ae401a92fe8aa1ecc86f2c00
Binh Do Quoc <binhdq@fpt.edu.vn>
2019-05-04 10:08:46 +0700

    1st created

---
+++ insertuser.php
@@ -0,0 +1,35 @@
+<html>
+<body>
+
+<form method="post">
+	id: <input type="text" name="id"/><br>
+	Name: <input type="text" name="name"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
+<?php
+	if (isset($_POST["id"]) && isset($_POST["name"])) {
+		var $id = $_POST["id"];
+		var $name = $_POST["name"];
+		//khoi tao ket noi den csdl
+		$db = parse_url(getenv("DATABASE_URL"));
+   		$pdo = new PDO("pgsql:" . sprintf(
+        	"host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")));
+
+        $data = [
+		    'id' => $id,
+		    'name' => $name
+		];
+		$sql = "INSERT INTO label (id, name) VALUES (:id, :name)";
+		$stmt= $pdo->prepare($sql);
+		$stmt->execute($data);
+
+	}
+?>
